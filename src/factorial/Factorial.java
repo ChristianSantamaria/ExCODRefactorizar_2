9a888ac02532077d4fc7c79546eeb22e48405e46
csantamariacameselle <csantamariacameselle@atanasoff19.danielcastelao.org>
2016-12-12 09:19:39 +0100

    Commit Format

---
+++ src/factorial/Factorial.java
@@ -0,0 +1,27 @@
+package factorial;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        //Hacemos click derecho Format
+        
+        int j;
+        int f;
+
+        j = 8;
+
+        int i;
+        if (j == 0) {
+            f = 1;
+        } else {
+            f = 1;
+            for (i = j; i >= 1; i--) {
+                f = f * i;
+            }
+        }
+
+        System.out.println(f);
+
+    }
+
+}
