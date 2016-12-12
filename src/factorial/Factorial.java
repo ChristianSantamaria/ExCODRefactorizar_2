4890c795eb3775a579c2fbf1762f28d88a0f51be
csantamariacameselle <csantamariacameselle@atanasoff19.danielcastelao.org>
2016-12-12 09:26:36 +0100

    Commit Metodos

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -2,26 +2,32 @@ package factorial;
 
 public class Factorial {
 
-    public static void main(String[] args) {
-        //Hacemos click derecho Format
-        //Hacemos el Rename
-        
-        int j;
+    public static void calcular() {
+        int j = 8;
         int f;
 
-        j = 8;
-
-        int i;
         if (j == 0) {
             f = 1;
         } else {
             f = 1;
-            for (i = j; i >= 1; i--) {
-                f = f * i;
-            }
+            bucle(f, j);
         }
-
+        
         System.out.println(f);
+    }
+
+    public static void bucle(int f, int j) {
+        for (int i = j; i >= 1; i--) {
+            f = f * i;
+        }
+    }
+
+    public static void main(String[] args) {
+        //Hacemos click derecho Format
+        //Hacemos el Rename
+        //Creo dos metodos uno para la formula y el otro para el bucle
+
+        calcular();
 
     }
 
